d819b0ececa4cbc4594dfb076005b79632a1010b
so-zero <zero_s0@naver.com>
2024-07-22 16:47:31 +0900

    react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <div>Home</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
