d1206ae2c6f59daec0c24469eb90cba0948d4eef
so-zero <zero_s0@naver.com>
2024-07-24 23:14:44 +0900

    server login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const HttpError = require("../models/errorModel");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Register
 async function register(req, res, next) {
@@ -36,6 +37,43 @@ async function register(req, res, next) {
   }
 }
 
+// Login
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new HttpError("모든 필드를 입력해 주세요.", 400));
+    }
+
+    const newEmail = email.toLowerCase();
+
+    const user = await User.findOne({ email: newEmail });
+    if (!user) {
+      return next(new HttpError("잘못된 이메일주소입니다.", 400));
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return next(new HttpError("잘못된 비밀번호입니다.", 400));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(201)
+      .json({ message: "로그인 되었습니다.", rest });
+  } catch (error) {
+    return next(
+      new HttpError("로그인에 실패했습니다. 계정을 다시 확인해 주세요.", 400)
+    );
+  }
+}
+
 module.exports = {
   register,
+  login,
 };
