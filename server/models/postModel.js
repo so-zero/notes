7290ae87b3372d78db51f5fadc8db4e1e30aece8
so-zero <zero_s0@naver.com>
2024-07-25 17:01:07 +0900

    server postModel

---
+++ server/models/postModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    isImportant: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
